c9b69c75df585f00dfe201c10b12cea02b4e814b
mfyu <myu121@gmail.com>
2019-07-17 20:49:25 -0700

    a

--- knight/knight.rb
+++ knight/knight.rb
@@ -13,24 +13,40 @@ class KnightPathFinder
         arr = []
         row, col = pos
         arr<<[row+2, col+1] if row+2<8 && col+1<8
-        arr<<[row+2, col-1] if row+2<8 && col-1>0
-        arr<<[row-2, col-1] if row-2>0 && col-1>0
-        arr<<[row-2, col+1] if row-2>0 && col+1<8
-        arr<<[row-1, col+2] if row-1>0 && col+2<8
-        arr<<[row-1, col-2] if row-1>0 && col-2>0
+        arr<<[row+2, col-1] if row+2<8 && col-1>=0
+        arr<<[row-2, col-1] if row-2>=0 && col-1>=0
+        arr<<[row-2, col+1] if row-2>=0 && col+1<8
+        arr<<[row-1, col+2] if row-1>=0 && col+2<8
+        arr<<[row-1, col-2] if row-1>=0 && col-2>=0
         arr<<[row+1, col+2] if row+1<8 && col+2<8
-        arr<<[row+1, col-2] if row+1<8 && col-2>0
-
-
-
-
+        arr<<[row+1, col-2] if row+1<8 && col-2>=0
         return arr
     end
+
+    def new_move_positions(pos)
+        arr = []
+        moves = KnightPathFinder.valid_moves(pos)
+        moves.each do |move|
+            if !@considered_positions.include?(move)
+                arr<<move 
+                @considered_positions<<move
+            end
+        end
+        return arr
+    end
+
 
 end
 
 kpf = KnightPathFinder.new([0,0])
-print kpf.root_node
-puts
 
-print KnightPathFinder.valid_moves(kpf.root_node)
+
+
+print KnightPathFinder.valid_moves([1,2])
+
+puts
+print kpf.considered_positions
+puts
+print kpf.new_move_positions([1,2])
+puts
+print kpf.considered_positions
