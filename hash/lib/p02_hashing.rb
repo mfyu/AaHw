7a8a384ea098b28c8725c1ebf753650184977aa9
mfyu <myu121@gmail.com>
2019-07-29 18:54:48 -0700

    hash

--- hash/lib/p02_hashing.rb
+++ hash/lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    out = 0
+    self.each_with_index do |e,i|
+      out += e*i
+    end
+    out
   end
 end
 
 class String
   def hash
+    out = 0
+    self.each_byte.with_index do |e,i|
+      out += e*i
+    end
+    out
   end
 end
 
@@ -16,6 +26,13 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    out = 0
+    self.each do |k,v|
+      out += (k.hash + v.hash)
+    end
+
+    out
   end
 end
+
+puts "abc".hash
